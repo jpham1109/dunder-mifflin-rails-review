d882ec870cffaa2ad0fd85f82b371eb4ae449bdc
Jamie H Pham <jpham1109@gmail.com>
2021-03-01 20:51:54 -0500

    Done except for the bonus

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,22 @@
 class DogsController < ApplicationController
+    before_action :find_dog, only: [:show, :sort]
+     
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        
+    end
+
+    def sort
+        @sorted_dogs = Dog.all.sort_dog
+        redirect_to sort_path
+    end
+
+    private
+
+    def find_dog
+        @dog = Dog.find(params[:id])
+    end
 end
